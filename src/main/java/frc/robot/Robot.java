64423ffc285656023c38dbfb0c208d3804fbb8ed
LamaOstrich <kaijvw@gmail.com>
2024-10-26 11:36:16 -0400

    fixed auto

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -101,7 +101,7 @@ public class Robot extends TimedRobot {
       case kDefaultAuto:
       default:
         _drivetrain.drive(.8, 0);
-        Timer.delay(2);
+        Timer.delay(.7);
         _drivetrain.drive(0, 0);
         break;
     }
